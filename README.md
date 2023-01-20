1f12a351a1c1a2d095f0cced8aee5432a0cf371f
akvadrako <dev@doubly.so>
2023-01-20 17:10:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sv-bug-fetch-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sv-bug-fetch-demo)
---
+++ src/routes/Fetcher.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let body = '...loading...';
+    
+    onMount(async () => {
+        let resp = await fetch(`https://api.iconify.design/carbon.json?icons=sun`);
+        body = JSON.stringify(await resp.json(), null, 4);
+    })
+</script>
+
+<pre>{body}</pre>
